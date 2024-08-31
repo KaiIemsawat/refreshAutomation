ebc67d5df137396b91cdfa6b0f55f4d4d3503088
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-08-31 17:05:59 -0400

    wrap up first step testing

--- src/test/java/d21/TestCase01.java
+++ src/test/java/d21/TestCase01.java
@@ -6,10 +6,21 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class TestCase01 {
   public static void main(String[] args) {
 
-    //    Launch browser
+    //    1. Launch browser
     WebDriver driver = new ChromeDriver();
 
-    //    Open URL on te browser (use 'https://demo.opencart.com/')
+    //    2. Open URL on te browser (use 'https://demo.opencart.com/')
     driver.get("https://demo.opencart.com/");
+
+    //    3. Validate title
+    String actualTitle = driver.getTitle();
+    if (actualTitle.equals("Your Store")) {
+      System.out.println("Test Passed");
+    } else {
+      System.out.println("Test Failed");
+    }
+    //  4. Close/Quit browser
+    driver.quit();
+    //    driver.close();
   }
 }
